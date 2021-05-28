73b62f59a7e737581356ef6dcfe23b2500519a56
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-28 18:41:12 -0400

    Mod Elements and Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+// const newHeader = document.createElement('h1')
+ //document.body.appendChild(newHeader)
+ 
+ let newHeader = document.createElement("h1")
+    newHeader.setAttribute("id", "victory")
+    document.body.appendChild(newHeader)  
+
+let champD = document.getElementById("victory").textContent += "Drew is the champion";
+
+ 
+    //var node = document.createElement("LI");                 // Create a <li> node
+    //var textnode = document.createTextNode("Water");         // Create a text node
+    //node.appendChild(textnode);                              // Append the text to <li>
+    //document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
